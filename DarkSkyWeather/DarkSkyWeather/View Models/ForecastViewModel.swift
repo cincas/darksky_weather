d9d53c820f6bb725bfd5b8f032a912accd608b3a
Tyler Yang <tyler.yang.xt@gmail.com>
2017-10-15 13:49:08 +1100

    Add ForecastViewModel

---
+++ DarkSkyWeather/DarkSkyWeather/View Models/ForecastViewModel.swift
@@ -0,0 +1,35 @@
+//  Copyright © 2017 Tyler Yang. All rights reserved.
+
+import Foundation
+import Result
+
+class ForecastViewModel {
+  fileprivate let apiClient = DarkSkyAPIClient()
+  var model: Forecast?
+  let latitude = 33.8650
+  let longitude = 151.2094
+}
+
+extension ForecastViewModel {
+  func load(_ completionHandler: @escaping (Result<Forecast, APIError>) -> Void) {
+    apiClient.fetchForecastWith(latitude: latitude, longitude: longitude) { [weak self] result in
+      guard let sself = self else {
+        completionHandler(.failure(.unknown))
+        return
+      }
+      
+      defer {
+        completionHandler(result)
+      }
+      
+      switch result {
+      case let .success(forecast):
+        sself.model = forecast
+        break
+      case let .failure(error):
+        print(error)
+        break
+      }
+    }
+  }
+}
--- DarkSkyWeather/DarkSkyWeather/ViewController.swift
+++ DarkSkyWeather/DarkSkyWeather/ViewController.swift
@@ -3,11 +3,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+  let viewModel = ForecastViewModel()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    let apiClient = DarkSkyAPIClient()
-    apiClient.fetchForecastWith(latitude: 33.8650, longitude: 151.2094) { result in
-      print(result)
-    }
   }
 }
